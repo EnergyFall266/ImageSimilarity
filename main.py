bf37823ef7b8ab13ae48a029169f6a7735309695
Leonardo Vanzin <leonardo.vanzin@hotmail.com>
2024-05-28 10:04:55 -0300

    compile no search

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ class SearchImage(BaseModel):
     image: str
 class Cleardata(BaseModel):
     clear: str
-class CompileImages(BaseModel):
-    compile: str
+
 images = []
 app = FastAPI()
 origins = ["*"]
@@ -47,9 +46,19 @@ async def add_image(image: List[AddImage]):
     
     return {"status": "success"}
 
-@app.post("/compile_images")
-async def compile_images(data:CompileImages):
-    
+
+
+@app.post("/clear_data")
+async def clear_data(data: Cleardata):
+
+    for root, dirs, files in os.walk('./images'):
+        for file in files:
+            os.remove(root  + '/'+ file)
+    return {"status": "cleared"}
+
+@app.post("/search_image")
+async def search_image(data: SearchImage):
+# compile all images in the folder
     for root, dirs, files in os.walk('./images'):
         for file in files:
             if file.endswith('jpg'):
@@ -74,19 +83,9 @@ async def compile_images(data:CompileImages):
         add_vector_to_index( features.mean(dim=1), index)
 
     faiss.write_index(index,"vector.index")
-        
-    return {"status": "compiled"}
 
-@app.post("/clear_data")
-async def clear_data(data: Cleardata):
+# search for the image
 
-    for root, dirs, files in os.walk('./images'):
-        for file in files:
-            os.remove(root  + '/'+ file)
-    return {"status": "cleared"}
-
-@app.post("/search_image")
-async def search_image(data: SearchImage):
     image = base64.b64decode(data.image)
     with open("search.jpg", "wb") as f:
         f.write(image)
@@ -103,14 +102,18 @@ async def search_image(data: SearchImage):
 
     index = faiss.read_index("vector.index")
     print(images)
+    print(len(images))
     if len(images)<5:
         k= len(images)
     else:
         k=5
     d,i = index.search(vector,k)
     print('Images:', [images[index] for index in i[0]])
-    for root, dirs, files in os.walk(images):
-        for file in files:
-            print( file)
 
-    return {"status": "success", "result": [images[index] for index in i[0]]}
+    # Convert images to base64
+    base64_images = []
+    for index in i[0]:
+        with open(images[index], "rb") as img_file:
+            base64_images.append(base64.b64encode(img_file.read()).decode('utf-8'))
+
+    return {"status": "success", "result": base64_images}
